50e4edb3ff69a342122351e92ee948636a98262b
S Chathuranga Jayasinghe <89441619+s-chathuranga-j@users.noreply.github.com>
2022-02-28 00:46:00 +0530

    Updated the BaseAPITests

--- ArvatoAPI/BaseAPI/BaseAPITests.cs
+++ ArvatoAPI/BaseAPI/BaseAPITests.cs
@@ -11,16 +11,29 @@ public class BaseAPITests
     public static RestRequest Request;
     public static IRestResponse Response;
     
+    /// <summary>
+    /// Sets the base URL to the rest client
+    /// </summary>
     public static void SetBaseUrl()
     {
         Client = new RestClient(BaseURL);
     }
 
+    /// <summary>
+    /// Sets the URI of the request where the request to directed to
+    /// </summary>
+    /// <param name="uri"></param>
     public static void SetRequestURI(string uri)
     {
         Request.Resource = "api/v3/validate/bank-account";
     }
 
+    /// <summary>
+    /// Maps the integer status code sent via the feature file to the HttpStatusCode
+    /// and returns the HttpStatusCode accordingly
+    /// </summary>
+    /// <param name="statusCodeString"></param>
+    /// <returns></returns>
     public static HttpStatusCode getStatusCode(int statusCodeString)
     {
         HttpStatusCode statusCode;
@@ -42,6 +55,12 @@ public class BaseAPITests
         return statusCode;
     }
 
+    /// <summary>
+    /// Deserializes the json response to a given POCO(Plain Old Csharp Object)
+    /// and returns the relevant class instance
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
     public static T DeserializeResponse<T>()
     {
         JsonDeserializer jsonDeserializer = new JsonDeserializer();
